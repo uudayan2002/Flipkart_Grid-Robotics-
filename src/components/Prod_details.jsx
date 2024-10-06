48a3dfafd44cadb6009eaaa27dcac4e46e8871c6
Udayan <nathuk50@gmail.com>
2024-10-06 12:30:30 +0530

    Second Commit

---
+++ src/components/Prod_details.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Box, Text, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
+
+function Prod_details() {
+  return (
+    <Box bg="#40513B" borderRadius="20px" border="2px solid #EDF1D6" color="#EDF1D6" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" gridRow="span 2" p={4} position="relative">
+
+      
+
+      <Accordion defaultIndex={[0]} allowMultiple width="100%" mt="80px">
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                <Box position="absolute" top="5px" right="10px" width="50px" height="50px" opacity={1} display="flex" alignItems="center" justifyContent="center" border="2px solid #EDF1D6" borderRadius="10px" color="#EDF1D6" fontSize="20px" fontWeight="bold" bg="#40513B">
+                  A
+                </Box>
+                Maggi 70g
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 2 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
+  );
+}
+
+export default Prod_details;
