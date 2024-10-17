be3cfd2fb7395fc8c08c332669a73b143d5caa68
Utsav1256 <utsavkumar5462@gmail.com>
2024-10-17 10:14:35 +0530

    added checkbox for packed and unpacked

--- client/src/components/Prod_details.jsx
+++ client/src/components/Prod_details.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import { Box, Text, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Box,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Checkbox,
+  Stack,
+} from '@chakra-ui/react';
 
 function Prod_details() {
+  const [isPacked, setIsPacked] = useState(false);
+  const [isUnpacked, setIsUnpacked] = useState(false);
+
+  const handlePackedChange = () => {
+    setIsPacked(!isPacked);
+    if (isUnpacked) setIsUnpacked(false); 
+  };
+
+  const handleUnpackedChange = () => {
+    setIsUnpacked(!isUnpacked);
+    if (isPacked) setIsPacked(false); 
+  };
   return (
     <Box bg="#40513B" borderRadius="20px" border="2px solid #EDF1D6" color="#EDF1D6" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" gridRow="span 2" p={4} position="relative">
-
-      <Accordion defaultIndex={[0]} allowMultiple width="100%" mt="80px">
+<Stack direction="row" spacing={4} mt="20px" align="center">
+        <Checkbox isChecked={isPacked} onChange={handlePackedChange}>
+          Packed
+        </Checkbox>
+        <Checkbox isChecked={isUnpacked} onChange={handleUnpackedChange}>
+          UnPacked
+        </Checkbox>
+      </Stack>
+      <Accordion defaultIndex={[0]} allowMultiple width="100%" mt="20px">
+        
         <AccordionItem>
           <h2>
             <AccordionButton>
+              
               <Box as="span" flex="2" textAlign="left">
-                <Box position="relative" top="5px" left="300px" width="50px" height="50px" opacity={1} display="flex" alignItems="center" justifyContent="center" border="2px solid #EDF1D6" borderRadius="10px" color="#EDF1D6" fontSize="20px" fontWeight="bold" bg="#40513B">
-                  A
-                </Box>
+               
                 <Text fontSize='xl' as='b'>Maggi 70g</Text>
               </Box>
               <AccordionIcon />
